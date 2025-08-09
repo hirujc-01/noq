77cb27d10aaeab8258fe2a9c1324f975135c5310
hirujc-01 <hiranvinaayak04@gmail.com>
2025-08-09 13:57:11 +0800

    adding workflow files

---
+++ buildnrun.sh
@@ -0,0 +1,9 @@
+set -e
+
+clear
+make clean
+echo "[Build has been cleaned 1/2]"
+make
+echo "[OS is compiled 2/2]"
+echo "[OS is ready for launch!]"
+qemu-system-i386 -fda main_flppy.img
--- getandrun.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-set -e
-rm main_floppy.img
-scp hirujc@192.168.1.85:/home/hirujc/noq/build/main_floppy.img .
-qemu-system-i386 -fda main_floppy.img
